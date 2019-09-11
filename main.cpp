f912f88915504c71a9eb81dc71cdf9d4476f23a4
kelldeveloped <48416779+kelldeveloped@users.noreply.github.com>
2019-09-11 11:11:27 -0400

    Introduced variables into main function

--- main.cpp
+++ main.cpp
@@ -6,11 +6,18 @@ int main() // this is a function in c++!
 {
     // cout << "Hello world" << endl;
     // cout << "Kelly" << endl;
-    cout << "   /|" << endl;
-    cout << "  / |" << endl;
-    cout << " /  |" << endl;
-    cout << "/___|" << endl;
+    string characterName = "Nancy";
+    int characterAge = 50;
+    cout << "There once was a woman named " << characterName << endl;
+    cout << "She was " << characterAge << " years old" << endl;
+    characterName = "Kathy";
+    characterAge = 55;
+    cout << "She liked the name " << characterName << endl;
+    cout << "And enjoyed being " << characterAge << endl;
     return 0;
 }
 
-// cout = console out endl = end line
+// cout = console out -> endl = end line
+// int -> integer -> whole number
+// int characterAge;
+// characterAge = 50; is also valid syntax
