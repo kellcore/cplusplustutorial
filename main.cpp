ec9474bf3dd2384f8ef952ef06ae60ab222664f2
kelldeveloped <48416779+kelldeveloped@users.noreply.github.com>
2019-09-11 10:26:25 -0400

    Created triangle shape and added notes

--- main.cpp
+++ main.cpp
@@ -2,8 +2,15 @@
 
 using namespace std;
 
-int main()
+int main() // this is a function in c++!
 {
-    cout << "Hello world" << endl;
+    // cout << "Hello world" << endl;
+    // cout << "Kelly" << endl;
+    cout << "   /|" << endl;
+    cout << "  / |" << endl;
+    cout << " /  |" << endl;
+    cout << "/___|" << endl;
     return 0;
 }
+
+// cout = console out endl = end line
