1c397a30dac27b0820e8764d8bc91968aad35b09
kelldeveloped <48416779+kelldeveloped@users.noreply.github.com>
2019-09-11 11:21:26 -0400

    Added notes about data types

--- main.cpp
+++ main.cpp
@@ -6,18 +6,29 @@ int main() // this is a function in c++!
 {
     // cout << "Hello world" << endl;
     // cout << "Kelly" << endl;
-    string characterName = "Nancy";
-    int characterAge = 50;
-    cout << "There once was a woman named " << characterName << endl;
-    cout << "She was " << characterAge << " years old" << endl;
-    characterName = "Kathy";
-    characterAge = 55;
-    cout << "She liked the name " << characterName << endl;
-    cout << "And enjoyed being " << characterAge << endl;
+    // string characterName = "Nancy";
+    // int characterAge = 50;
+    // cout << "There once was a woman named " << characterName << endl;
+    // cout << "She was " << characterAge << " years old" << endl;
+    // characterName = "Kathy";
+    // characterAge = 55;
+    // cout << "She liked the name " << characterName << endl;
+    // cout << "And enjoyed being " << characterAge << endl;
+
+    char grade = 'A';
+    string phrase = "It is your birthday.";
+    int age = 32;
+    // float -> floating point
+    double gpa = 3.5;
+    bool isHere = true;
+
+    cout << phrase << endl;
+
     return 0;
 }
+// char -> character
 
 // cout = console out -> endl = end line
-// int -> integer -> whole number
+// int -> integer -> whole number -> no decimals!
 // int characterAge;
 // characterAge = 50; is also valid syntax
